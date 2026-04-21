8e145afc6e4a7fbe03f8225ba90a85d2bd32f797
emyuaar <minhajurrehman32@gmail.com>
2026-04-21 17:44:37 +0500

    LMS FILE UPLOAD NEW FEATURE 1

--- resources/views/learner/courses/show.blade.php
+++ resources/views/learner/courses/show.blade.php
@@ -177,13 +177,14 @@
                                                         </div>
                                                         <div class="flex items-center gap-2 pl-12 md:pl-0">
                                                             <x-ui.button href="{{ $primaryUrl }}" target="{{ $primaryTarget }}"
-                                                                size="sm" variant="{{ $isFileResource ? 'primary' : 'outline' }}"
-                                                                icon="{{ $btnIcon }}">
+                                                                size="sm" variant="{{ $isFileResource ? 'primary' : 'outline' }}">
+                                                                @if($btnIcon === 'download')
+                                                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-4 h-4 mr-2"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path><polyline points="7 10 12 15 17 10"></polyline><line x1="12" y1="15" x2="12" y2="3"></line></svg>
+                                                                @endif
                                                                 {{ $actionLabel }}
                                                             </x-ui.button>
                                                         </div>
                                                     </div>
->
                                                 @endforeach
                                             </div>
                                         </div>
