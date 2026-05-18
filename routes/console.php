651decdf279778ae5d6a12e054f41d4087501ca3
emyuaar <minhajurrehman32@gmail.com>
2026-05-18 13:24:41 +0500

    learner submission email setup 1

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,8 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// Automatically process queued jobs (like assignment submission emails) every minute via the scheduler
+\Illuminate\Support\Facades\Schedule::command('queue:work --stop-when-empty')
+    ->everyMinute()
+    ->withoutOverlapping();
